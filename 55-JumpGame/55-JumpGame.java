701f73b8d377e963a52ec4c5fdb022d705818ce2
Rinki4738 <rinkijha3315@gmail.com>
2025-08-14 15:48:58 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,11 @@
+// Last updated: 8/14/2025, 3:48:57 PM
+class Solution {
+    public boolean canJump(int[] nums) {
+       int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>max)
+            return false;
+            max=Math.max(max,i+nums[i]);
+        }
+        return true;
+    }}
