9afa780c19eb0e643271de38bf2bd437b5f648a5
Rinki4738 <rinkijha3315@gmail.com>
2025-08-14 15:45:41 +0530

    Create 524-LongestWordInDictionaryThroughDeleting

---
+++ 524-LongestWordInDictionaryThroughDeleting/524-LongestWordInDictionaryThroughDeleting.java
@@ -0,0 +1,26 @@
+// Last updated: 8/14/2025, 3:45:40 PM
+class Solution {
+    public String findLongestWord(String s, List<String> dictionary) {
+        String ans="";
+        Collections.sort(dictionary);
+        for(int i=0;i<dictionary.size();i++){
+            String word=dictionary.get(i);
+            if(issubsequence(s,word)){
+                if(word.length()>ans.length()){
+                    ans=word;
+                }
+            }
+        }return ans;
+
+        
+    }public static boolean issubsequence(String s,String word){
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<word.length()){
+            if(s.charAt(i)==word.charAt(j)){
+                j++;
+            }
+            i++;
+        } return j == word.length();
+    }
+}
