bc5c093271f5d4827a37edff2fca323582ec4b53
Rinki4738 <rinkijha3315@gmail.com>
2025-08-14 15:47:19 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.java
@@ -0,0 +1,28 @@
+// Last updated: 8/14/2025, 3:47:18 PM
+class Solution {
+    public int countPrimes(int n) {
+        if(n<=2){
+            return 0;
+        }
+        return primeSieve(n);
+        
+    }
+    public static int primeSieve(int n) {
+		int [] ans=new int[n];
+		ans[0]=ans[1]=1;
+		for(int i=2;i*i<=ans.length;i++) {
+			if(ans[i]==0) {// i prime hai
+				for(int j=2;i*j<ans.length;j++) {
+					ans[i*j]=1;
+				}
+				
+			}
+		}
+		int c=0;
+		for(int i=2;i<ans.length;i++) {
+			if(ans[i]==0)
+				c++;
+		}
+		return c;
+	}
+}
