e84aa492fdacb36ef2d8c5da442c10fe1fb7e7d2
Rinki4738 <rinkijha3315@gmail.com>
2025-08-15 22:00:37 +0530

    Create 2346-Largest3SameDigitNumberInString

---
+++ 2346-Largest3SameDigitNumberInString/2346-Largest3SameDigitNumberInString.java
@@ -0,0 +1,25 @@
+// Last updated: 8/15/2025, 10:00:36 PM
+class Solution {
+    public String largestGoodInteger(String s) {
+        int ans=0;
+        int flag=0;
+        for(int i=0;i<s.length()-2;){
+            if(s.charAt(i)==s.charAt(i+1) && s.charAt(i)==s.charAt(i+2)){
+                int k=s.charAt(i)-'0';
+                flag=1;
+               ans= Math.max(ans,k);
+               i+=3;
+
+            }
+            else{
+                i++;
+            }
+        }
+        if(flag==0){
+            return "";
+        }
+        return ans+""+ans+""+ans;
+        
+        
+    }
+}
