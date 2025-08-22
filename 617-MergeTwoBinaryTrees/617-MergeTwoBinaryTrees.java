cffcec76bd51219484b8036ab949ae2bb75fef6a
Rinki4738 <rinkijha3315@gmail.com>
2025-08-22 20:50:23 +0530

    File updated at 8/22/2025, 8:50:23 PM

--- 617-MergeTwoBinaryTrees/617-MergeTwoBinaryTrees.java
+++ 617-MergeTwoBinaryTrees/617-MergeTwoBinaryTrees.java
@@ -1,4 +1,4 @@
-// Last updated: 8/14/2025, 3:45:22 PM
+// Last updated: 8/22/2025, 8:50:23 PM
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -16,32 +16,18 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        if(root1==null && root2==null){
-            return null;
-        }
-        
-        TreeNode nn=new TreeNode();
-        int sum=0;
         if(root1==null){
-            sum=root2.val;
-            nn.val=sum;
-            nn.left=mergeTrees(null,root2.left);
-            nn.right=mergeTrees(null,root2.right);
+            return root2;
+        }
+        if(root2==null){
+            return root1;
+        }
 
-        }
-        else if(root2==null){
-            sum=root1.val;
-            nn.val=sum;
-            nn.left=mergeTrees(root1.left,null);
-            nn.right=mergeTrees(root1.right,null);
-        }
-        else{
-        sum=root1.val+root2.val;
-        nn.val=sum;
+        TreeNode nn=new TreeNode();
+        nn.val=root1.val+root2.val;
         nn.left=mergeTrees(root1.left,root2.left);
-        nn.right=mergeTrees(root1.right,root2.right);}
+        nn.right=mergeTrees(root1.right,root2.right);
         return nn;
-
         
     }
 }
