60a45cd5b73cf9ab010410b2afe6fda2a27b3cd0
Rinki4738 <rinkijha3315@gmail.com>
2025-08-29 21:01:18 +0530

    File updated at 8/29/2025, 9:01:17 PM

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,17 +1,16 @@
-// Last updated: 8/29/2025, 8:58:14 PM
+// Last updated: 8/29/2025, 9:01:17 PM
 class Solution {
     public int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-       int dp[]=new int[n+1];
-      
-      dp[0]=0;
-      dp[1]=1;
-      for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-      }
-      return dp[n];
+       int prev2=0;
+       int prev=1;
+       int i=0;
+       while(i<n){
+        int curr=prev+prev2;
+        prev2=prev;
+        prev=curr;
+        i++;
+       }
+       return prev2;
         
 
         
